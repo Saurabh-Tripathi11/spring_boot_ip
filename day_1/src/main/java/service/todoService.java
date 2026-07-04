c393cca5355d498309728f596ff7507b0ef198de
Saurabh-Tripathi11 <saurabh.g.tripathi06@gmail.com>
2026-07-04 16:55:10 +0530

    Add todo service CRUD methods
    Inject the todo repository into `todoService` and add methods to list, create, fetch by id, and delete todos.

--- day_1/src/main/java/service/todoService.java
+++ day_1/src/main/java/service/todoService.java
@@ -1,8 +1,35 @@
 package service;
 
+import entity.todoEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import repository.todoRepository;
+
+import java.util.List;
 
 @Service
 public class todoService {
+    @Autowired
+    private todoRepository todoRepository;
+
+    // Get all todos
+    public List<todoEntity> getAllTodos() {
+        return todoRepository.findAll();
+    }
+
+    // Save a new todo
+    public todoEntity saveTodo(todoEntity todo) {
+        return todoRepository.save(todo);
+    }
+
+    // Get todo by id
+    public todoEntity getTodoById(Long id) {
+        return todoRepository.findById(id).orElse(null);
+    }
+
+    // Delete todo
+    public void deleteTodo(Long id) {
+        todoRepository.deleteById(id);
+    }
 
 }
